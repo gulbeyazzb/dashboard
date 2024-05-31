a4c359aadf2103c84ea7813f24ef5fc335f52bf7
Gulbeyaz <glbzbayram@gmail.com>
2024-05-31 14:09:23 +0300

    Side Bar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,24 +1,25 @@
 import { FaHome } from "react-icons/fa";
 import { BsFillCreditCardFill, BsPersonFill } from "react-icons/bs";
 import { HiOutlineLogout } from "react-icons/hi";
+import { cn } from "@/utils/tailwind-util";
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <button className="bg-white">
-        <img src="images/Rising Logo.svg"></img>
-      </button>
+    <div className="flex flex-col gap-4 items-center p-6">
       <button>
-        <FaHome color="#7E868C" className="text-2xl" />
+        <img className="w-[54px] h-[55px]" src="images/Rising Logo.svg"></img>
       </button>
-      <button>
-        <BsFillCreditCardFill color="#7E868C" className="text-md" />
+      <button className="sideBar-btn">
+        <FaHome color="#7E868C" className="sideBar-icons" />
       </button>
-      <button>
-        <BsPersonFill color="#7E868C" className="text-md" />
+      <button className="sideBar-btn">
+        <BsFillCreditCardFill color="#7E868C" className="sideBar-icons" />
       </button>
-      <button>
-        <HiOutlineLogout color="#7E868C" className="text-md" />
+      <button className="sideBar-btn">
+        <BsPersonFill color="#7E868C" className="sideBar-icons" />
+      </button>
+      <button className="sideBar-btn">
+        <HiOutlineLogout color="#7E868C" className="sideBar-icons" />
       </button>
     </div>
   );
