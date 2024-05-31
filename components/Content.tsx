b10fb287359fffecc3e8502503ee3d1365860fc9
Gulbeyaz <glbzbayram@gmail.com>
2024-05-31 14:09:15 +0300

    Content Bar

---
+++ components/Content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Home";
+
+const Content = () => {
+  return (
+    <div className="flex-1">
+      <Home />
+    </div>
+  );
+};
+
+export default Content;
