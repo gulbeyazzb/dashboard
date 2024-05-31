ca4c4fc116d07322e878b297e59b7e540f0d2f51
Gulbeyaz <glbzbayram@gmail.com>
2024-05-31 16:07:11 +0300

    Status data was fetched with get-info  endpoint.
    format was changed by Format usage function

---
+++ components/Status.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { API } from "@/api/api";
+import { Alert } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+type initials = {
+  dailyUsage: string;
+  expireTime: string;
+  lastCharge: string;
+  lastChargeAmount: string;
+  totalDataUsage: string;
+};
+
+const Status = () => {
+  const [isClient, setIsClient] = useState<boolean>(false);
+  const [data, setData] = useState<initials | undefined>(undefined);
+  const [err, setErr] = useState<string>("");
+
+  const formatUsage = (usage: string) => {
+    const usageNumber = Number(usage);
+    if (isNaN(usageNumber)) return usage;
+    return (usageNumber / 1000).toLocaleString("en-EN", {
+      minimumFractionDigits: 3,
+      maximumFractionDigits: 3,
+    });
+  };
+
+  useEffect(() => {
+    setIsClient(true);
+    if (localStorage.getItem("token")) {
+      API.get("get-info")
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch((e) =>
+          setErr(
+            "Veri çekilirken bir hata ile karşılaşıldı.. Lütfen daha sonra tekrar deneyiniz, veya sorunu bildiriniz."
+          )
+        );
+    }
+  }, []);
+  return (
+    <div>
+      {err && (
+        <div>
+          <Alert severity="error">{err}</Alert>
+        </div>
+      )}
+      {data ? (
+        <div className="max-w-4xl mx-auto">
+          <div className="flex gap-7 py-16">
+            <div className="bg-status_bg_blue rounded-2xl">
+              <div className="flex flex-col gap-2 p-6">
+                <h3 className="status-headers font-semibold ">
+                  Subscription expires on
+                </h3>
+                <span className="status-spans font-medium">
+                  {data.expireTime}
+                </span>
+              </div>
+            </div>
+            <div className="bg-status_bg_gray rounded-2xl">
+              <div className="flex flex-col gap-2 p-6">
+                <h3 className="status-headers font-semibold">Last charge</h3>
+                <span className="status-spans font-medium">
+                  {data.lastChargeAmount}{" "}
+                  <span className="status-headers font-medium">
+                    {data.lastCharge}
+                  </span>
+                </span>
+              </div>
+            </div>
+            <div className="bg-status_bg_blue rounded-2xl">
+              <div className="flex flex-col gap-2 p-6">
+                <h3 className="status-headers font-bold">Total Usage Data</h3>
+                <span className="status-spans font-bold">
+                  {formatUsage(data.totalDataUsage)} GB
+                </span>
+              </div>
+            </div>
+            <div className="bg-status_bg_gray rounded-2xl">
+              <div className="flex flex-col gap-2 p-6">
+                <h3 className="status-headers font-bold">Daily Usage Data</h3>
+                <span className="status-spans font-bold">
+                  {formatUsage(data.dailyUsage)} GB
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>Veri yükleniyor...</div>
+      )}
+    </div>
+  );
+};
+
+export default Status;
