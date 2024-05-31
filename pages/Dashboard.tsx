f0c038ba1a177373a8fa153aebe0265a7e04b219
Gulbeyaz <glbzbayram@gmail.com>
2024-06-01 01:25:01 +0300

    if there is not token, user is routed to LoginPage and return toast message

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,24 +1,46 @@
+"use client";
 import Content from "@/components/Content";
 import SideBar from "@/components/SideBar";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { toast, Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Dashboard: React.FC = () => {
   const [isClient, setIsClient] = useState<boolean>(false);
+  const router = useRouter();
 
   useEffect(() => {
-    setIsClient(true);
+    if (localStorage.getItem("token")) {
+      setIsClient(true);
+    } else {
+      toast.error("Login Sayfasına Yönlendiriliyorsunuz...");
+      setTimeout(() => {
+        router.push("/");
+      }, 3000);
+    }
   }, []);
 
   return (
     <div className="bg-primary min-h-screen ">
-      {isClient ? (
+      <ToastContainer
+        position="top-center"
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        transition={Bounce}
+      />
+      {isClient && (
         <div className="flex">
           <SideBar />
           <div className="border border-1 border-[#E6E8EB] mr-4"></div>
           <Content />
         </div>
-      ) : (
-        "Loading..."
       )}
     </div>
   );
