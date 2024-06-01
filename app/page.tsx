9bc77f5e0f00c91c294d7b530af9b4a8a8a5fbb9
Gulbeyaz <glbzbayram@gmail.com>
2024-06-02 02:38:39 +0300

    unnecessary calling deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Dashboard from "@/pages/Dashboard";
 import LoginPage from "@/pages/LoginPage";
-
 export default function Home() {
   return (
     <>
       <LoginPage />
-      <Dashboard />
     </>
   );
 }
