f031e76d40703a8f553664e394655b9fbd9bf480
Gulbeyaz <glbzbayram@gmail.com>
2024-06-01 03:14:15 +0300

    dropdownMenu compound was created for dropdown menu (actions)

---
+++ components/compounds/DropdownMenu.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { FaAngleUp, FaAngleDown } from "react-icons/fa6";
+
+export default function DropdownMenu() {
+  const [open, setOpen] = useState(false);
+
+  const openActionsHandle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="flex flex-col relative">
+      <button
+        onClick={openActionsHandle}
+        className={`${
+          open ? "text-actions_blue" : "text-black"
+        } w-20 flex gap-2 justify-center items-center border shadow-md p-1 rounded-md relative`}
+      >
+        <div className=" font-medium text-[10px]">Actions</div>
+        {!open ? <FaAngleDown /> : <FaAngleUp />}
+      </button>
+
+      {open && (
+        <div className="w-20 flex flex-col rounded-md border pr-1 absolute top-7 z-10 bg-white shadow-lg">
+          <button className="text-[8px] hover:bg-gray-200 rounded">
+            Processing
+          </button>
+          <button className="text-[8px] hover:bg-gray-200 rounded">
+            In Progress
+          </button>
+          <button className="text-[8px] hover:bg-gray-200 rounded">
+            Completed
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
