15817b8a812381d9c670236785d96d9b9a306b41
Gulbeyaz <glbzbayram@gmail.com>
2024-06-01 04:27:29 +0300

    content was defined by menu prop

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Home from "./Home";
 
-const Content = () => {
+interface ContentProps {
+  menu: string;
+}
+
+const Content: React.FC<ContentProps> = ({ menu }) => {
   return (
     <div className="flex-1">
-      <Home />
+      {menu === "home" && <Home />}
+      {menu === "user" && <div>user page</div>}
+      {menu === "card" && <div>card page</div>}
     </div>
   );
 };
