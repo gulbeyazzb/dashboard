0b102d23b1bdc1ee97d34cb7f79cddffcb00a51f
Gulbeyaz <glbzbayram@gmail.com>
2024-06-01 04:29:33 +0300

    onClick function was defined for sidebar menus

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,24 +1,43 @@
 import { FaHome } from "react-icons/fa";
 import { BsFillCreditCardFill, BsPersonFill } from "react-icons/bs";
 import { HiOutlineLogout } from "react-icons/hi";
-import { cn } from "@/utils/tailwind-util";
+import { useRouter } from "next/router";
 
-const SideBar = () => {
+interface SideBarProps {
+  setMenu: (menu: string) => void;
+}
+
+const SideBar: React.FC<SideBarProps> = ({ setMenu }) => {
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    const targetId = (e.currentTarget as HTMLElement).id;
+    setMenu(targetId);
+    if (targetId === "logout") {
+      localStorage.removeItem("token");
+      router.push("/");
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4 items-center p-6">
       <button>
-        <img className="w-[54px] h-[55px]" src="images/Rising Logo.svg"></img>
+        <img
+          className="w-[54px] h-[55px]"
+          src="images/Rising Logo.svg"
+          alt="Rising Logo"
+        />
       </button>
-      <button className="sideBar-btn">
+      <button onClick={handleClick} className="sideBar-btn" id="home">
         <FaHome color="#7E868C" className="sideBar-icons" />
       </button>
-      <button className="sideBar-btn">
+      <button onClick={handleClick} className="sideBar-btn" id="card">
         <BsFillCreditCardFill color="#7E868C" className="sideBar-icons" />
       </button>
-      <button className="sideBar-btn">
+      <button onClick={handleClick} className="sideBar-btn" id="user">
         <BsPersonFill color="#7E868C" className="sideBar-icons" />
       </button>
-      <button className="sideBar-btn">
+      <button onClick={handleClick} className="sideBar-btn" id="logout">
         <HiOutlineLogout color="#7E868C" className="sideBar-icons" />
       </button>
     </div>
