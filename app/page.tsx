d5398052558a1d6af90aed4f0299651ecf6811d5
Gulbeyaz <glbzbayram@gmail.com>
2024-06-02 02:23:16 +0300

    deneme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Dashboard from "@/pages/Dashboard";
 import LoginPage from "@/pages/LoginPage";
 
 export default function Home() {
   return (
     <>
       <LoginPage />
+      <Dashboard />
     </>
   );
 }
