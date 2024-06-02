57559a8802b55205be3a54125e38d438a9126131
Gulbeyaz <glbzbayram@gmail.com>
2024-06-02 03:24:14 +0300

    css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ module.exports = {
         white_secondary: "#E6E8EB",
         gray_primary: "#ECECEC",
         gray_secondary: "#7E868C",
-        logo_orange_color: "#d34e24",
+        logo_orange_color: "#f4ad25",
       },
     },
   },
